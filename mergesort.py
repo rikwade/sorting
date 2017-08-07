6fe83a27aca998a7ad15cb99ba7f31a183401365
Richard Wade <rik@rikwade.com>
2017-08-07 19:23:54 +0800

    added annotations

--- mergesort.py
+++ mergesort.py
@@ -16,25 +16,37 @@ def _mergesort(aList, first, last):
         _mergesort(aList, mid+1, last)
 
     # merge sorted lists
+    # a is temp list index
+    # f is left-hand list index
+    # l is right-hand list index
     a, f, l, = 0, first, mid+1
     tmp = [None] * ( last - first +1 )
 
+    # work along the left and right lists
     while f <= mid and l <= last:
         if aList[f] < aList[l]:
             tmp[a] = aList[f]
             f += 1
+            # left list was lowest, so copy it to the temp list
+            # go to next element in left list
         else:
             tmp[a] = aList[l]
             l += 1
+            # right list was lowest, so copy it to the temp list
+            # go to next element in right list
         a += 1
+        # go to next element in the temp list
 
+    # if there are any elements left in either list, copy them to the temp list
     if f <= mid:
         tmp[a:] = aList[f:mid +1]
     if l <= last:
         tmp[a:] = aList[l:last +1]
 
+    # reset the temp list index to the start
     a = 0
 
+    # copy the temp list to the main list
     while first <= last:
         aList[first] = tmp[a]
         first += 1
