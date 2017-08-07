8612de7f065059c996adf46788ae62362b0911e3
Richard Wade <rik@rikwade.com>
2017-08-07 11:55:17 +0800

    finalised quicksort

--- quicksort.py
+++ quicksort.py
@@ -3,13 +3,24 @@
 myList = [1,10,2,6,7,8,15,3,20,1000,5,9]
 
 # worst is O(n^2) average is O(n log n)
-def quicksort(sortList, start, end):
-    # we are down to 1 element partitions
-    if start >= end:
-        return sortList
-    pivot = end
+def quicksort(sortList):
+    less = []
+    equal = []
+    greater = []
 
+    if len(sortList) > 1:
+        pivot = sortList[0]
+        for x in sortList:
+            if x < pivot:
+                less.append(x)
+            if x == pivot:
+                equal.append(x)
+            if x > pivot:
+                greater.append(x)
+        return(quicksort(less)+equal+quicksort(greater))
+    else:
+        return sortList
 
 if __name__ == "__main__":
-    sortedList = bubblesort(myList)
+    sortedList = quicksort(myList)
     print(sortedList)
